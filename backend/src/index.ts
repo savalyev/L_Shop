9e6bde0d975ea9acca9d665f8d3b06dbe0300179
SVVVLYXV <dsaval89@gmail.com>
2026-02-23 23:39:48 +0300

    update: structure (#2)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,16 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript + Express!');
+  res.send('Первый HTTP запрос');
 });
 
+app.get('/user', (req: Request, res: Response) => {
+  res.send("Тут микрочелик...")
+})
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
