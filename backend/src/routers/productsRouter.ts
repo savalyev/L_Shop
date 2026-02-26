66f8c9bd93c4f06664e4879ed34c95dcc4475531
SVVVLYXV <dsaval89@gmail.com>
2026-02-26 20:28:39 +0300

    feat: update routers products (#8)

--- backend/src/routers/productsRouter.ts
+++ backend/src/routers/productsRouter.ts
@@ -8,6 +8,21 @@ const productsRouter = Router();
 //GET запрос, получение всех продуктов
 productsRouter.get('/', ProductsController.getAll);
 
+//GET запрос, получение продукта по имени
+productsRouter.get("/by-name", ProductsController.getByName);
+
+//GET запрос, получение продукта по описанию
+productsRouter.get("/by-description", ProductsController.getByDescription);
+
+//GET запрос, получение продуктов по возрастанию цены
+productsRouter.get("/ascending", ProductsController.getAllAscending);
+
+//GET запрос, получение продуктов по убыванию цены
+productsRouter.get("/descending", ProductsController.getAllDescending);
+
+//GET запрос, получение продуктов по фильтрам
+productsRouter.get("/fillters", ProductsController.getAllFiltered);
+
 // GET запрос, получение продукта по ID
 productsRouter.get('/:id', ProductsController.getById);
 
