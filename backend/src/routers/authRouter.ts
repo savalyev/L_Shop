40fdcfd8313799e107a755481a5afd97eeacc6df
SVVVLYXV <dsaval89@gmail.com>
2026-02-28 10:58:44 +0300

    Backend/users auth login (#12)
    * feat: update routers products
    
    * feat: add router register
    
    * feat: add router login

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -4,5 +4,6 @@ import { authController } from "../controllers/users/authController";
 const authRouter = Router();
 
 authRouter.post("/register", authController.register);
+authRouter.post('/login', authController.login);
 
 export {authRouter};
