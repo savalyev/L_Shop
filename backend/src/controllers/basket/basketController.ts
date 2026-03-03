b2b798a7fbd4d7e81a5e7960f2f2f8873d390b6f
ALDRKM <aleksej.lishtvan@gmail.com>
2026-03-03 21:04:32 +0300

    ff

--- backend/src/controllers/basket/basketController.ts
+++ backend/src/controllers/basket/basketController.ts
@@ -5,9 +5,9 @@ import * as path from 'path';
 
 
 export class BasketController{
-    public static async  GetBasket(req: Request, res: Response){
+    public static async  GetBasket(req: Request, res: Response): Promise<void>{
         try{
-
+            const userId = req.query.userId || req.params.userId;
         }
         catch(err){
             res.status(404).send({error:"not found"})
