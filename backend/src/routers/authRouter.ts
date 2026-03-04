858928f7cb303f37249e671bebce93c138ba73e2
SVVVLYXV <dsaval89@gmail.com>
2026-03-04 21:07:51 +0300

    Backend/update logout (#25)
    * feat: cleaning code
    
    * feat: add new endpoint, /logout and /me
    
    * feat: logout -> api/auth

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -5,5 +5,6 @@ const authRouter = Router();
 
 authRouter.post("/register", authController.register);
 authRouter.post('/login', authController.login);
+authRouter.post("/logout", authController.logout);
 
 export {authRouter};
