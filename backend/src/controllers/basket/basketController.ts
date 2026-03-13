698885c4f4bbc1e13a33cfd8bbe596878e7f8754
SVVVLYXV <dsaval89@gmail.com>
2026-03-14 00:29:48 +0300

    refactor: last update of most features (#49)

--- backend/src/controllers/basket/basketController.ts
+++ backend/src/controllers/basket/basketController.ts
@@ -3,18 +3,18 @@ import { BasketService } from '../../services/basket/basketService';
 
 export class BasketController {
 
-    public static async GetBasket(req: Request, res: Response): Promise<void> {
+    public static async getBasket(req: Request, res: Response): Promise<void> {
         try {
-            const userId = req.params.userId;
-            if (!userId) {
-                res.status(400).send({ error: "Обязательный параметр userId не указан" });
-                return;
-            }
-            const userbasket = await BasketService.GetBasketUserId(Number(userId));
+
+            const user = res.locals.user;
+
+            const userbasket = await BasketService.GetBasketUserId(Number(user?.id));
+
             if (!userbasket) {
                 res.status(404).send({ error: 'Корзина пользователя не найдена' });
                 return;
             }
+
             res.status(200).send(userbasket);
         }
         catch (err) {
@@ -22,14 +22,10 @@ export class BasketController {
         }
     }
 
-    public static async AddToBasket(req: Request, res: Response): Promise<void> {
+    public static async addToBasket(req: Request, res: Response): Promise<void> {
         try {
-            const userId = Number(req.body.userId);
 
-            if (!userId) {
-                res.status(404).send({ error: "Пользователь не найден" });
-                return;
-            }
+            const user = res.locals.user;
 
             const productId = Number(req.body.productId);
 
@@ -38,7 +34,7 @@ export class BasketController {
                 return;
             }
 
-            const userbasket = await BasketService.AddToBasket(userId, productId);
+            const userbasket = await BasketService.AddToBasket(Number(user?.id), productId);
 
             res.status(201).send({ userbasket });
         }
@@ -47,14 +43,10 @@ export class BasketController {
         }
     }
 
-    public static RemoveFromBasket(req: Request, res: Response): void {
+    public static removeFromBasket(req: Request, res: Response): void {
         try {
-            const userId = Number(req.body.userId);
 
-            if (!userId) {
-                res.status(404).send({ error: "user not found" });
-                return;
-            }
+            const user = res.locals.user;
 
             const productId = Number(req.body.productId);
 
@@ -62,7 +54,7 @@ export class BasketController {
                 res.status(404).send({ error: "product not found" });
             }
 
-            const userbasket = BasketService.RemoveFromBasket(userId, productId);
+            const userbasket = BasketService.RemoveFromBasket(Number(user?.id), productId);
 
             res.status(200).send(userbasket);
         }
@@ -71,14 +63,10 @@ export class BasketController {
         }
     }
 
-    public static RemoveProductFromBasket(req: Request, res: Response): void {
+    public static removeProduct(req: Request, res: Response): void {
         try {
-            const userId = Number(req.body.userId);
 
-            if (!userId) {
-                res.status(404).send({ error: "user not found" });
-                return;
-            }
+            const user = res.locals.user;
 
             const productId = Number(req.body.productId);
 
@@ -86,7 +74,7 @@ export class BasketController {
                 res.status(404).send({ error: "product not found" });
             }
 
-            const userbasket = BasketService.RemoveProductFromBasket(userId, productId);
+            const userbasket = BasketService.RemoveProductFromBasket(Number(user?.id), productId);
 
             res.status(200).send(userbasket);
         }
@@ -96,14 +84,10 @@ export class BasketController {
 
     }
 
-    public static RemoveAllBasket(req: Request, res: Response): void {
+    public static removeAllBasket(req: Request, res: Response): void {
         try {
-            const userId = Number(req.body.userId);
 
-            if (!userId) {
-                res.status(404).send({ error: "user not found" });
-                return;
-            }
+            const user = res.locals.user;
 
             const productId = Number(req.body.productId);
 
@@ -111,7 +95,7 @@ export class BasketController {
                 res.status(404).send({ error: "product not found" });
             }
 
-            const userbasket = BasketService.RemoveAllBasket(userId);
+            const userbasket = BasketService.RemoveAllBasket(Number(user?.id));
 
             res.status(200).send(userbasket);
         }
