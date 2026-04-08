e780f1aade469785511622f0db7d3780d3cae209
SVVVLYXV <dsaval89@gmail.com>
2026-04-08 15:34:35 +0300

    docs(auth): create swagger for register

--- backend/src/routers/authRouter.ts
+++ backend/src/routers/authRouter.ts
@@ -3,7 +3,69 @@ import { AuthController } from "../controllers/users/authController";
 
 const authRouter = Router();
 
+
+/**
+ * @openapi
+ * /api/auth/register:
+ *   post:
+ *     summary: Регистрация пользователя
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/RegistrationBody"
+ *     responses:
+ *       201:
+ *         description: Пользователь успешно зарегистрирован
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: "#/components/schemas/RequestUser"
+ *       400:
+ *         description: Имя и пароль обязательны
+ *       401:
+ *         description: Ошибка авторизации (AuthError)
+ *       500:
+ *         description: Внутренняя ошибка сервера
+ */
 authRouter.post("/register", AuthController.register);
+
+/**
+ * @openapi
+ * /api/auth/login:
+ *   post:
+ *     summary: Авторизация пользователя
+ *     tags:
+ *       - Auth
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/LoginBody"
+ *     responses:
+ *       201:
+ *         description: Пользователь успешно зарегистрирован
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   $ref: "#/components/schemas/RequestUser"
+ *       400:
+ *         description: Имя и пароль обязательны
+ *       401:
+ *         description: Ошибка авторизации (AuthError)
+ *       500:
+ *         description: Внутренняя ошибка сервера
+ */
 authRouter.post('/login', AuthController.login);
 authRouter.post("/logout", AuthController.logout);
 
